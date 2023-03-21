c16fe98886a3a36d0eec73cf26da1c65efbda3e6
silvsilvsilv <johnleonilsilva24@gmail.com>
2023-03-21 23:43:04 +0800

    changed it to Hello world

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,12 @@
 #include <SDL.h>
 #include <iostream>
-#include <SDL2_ttf.dll>
+
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << "SDL works" << endl;
+    cout << "hello world" << endl;
     cin.get();
 
     return 0;
