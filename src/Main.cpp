8d1f639cebc30b8036eb9ca20df3f58c30baf1ad
silvsilvsilv <johnleonilsilva24@gmail.com>
2023-03-21 21:15:34 +0800

    initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include <iostream>
+#include <SDL2_ttf.dll>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "SDL works" << endl;
+    cin.get();
+
+    return 0;
+}
