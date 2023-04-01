a4eb088fd62b22477cf0090858641610ec807bf2
silvsilvsilv <johnleonilsilva24@gmail.com>
2023-04-01 20:49:46 +0800

    main game is done, TODO: add game over screen and difficulty incrementation

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,52 @@
 #include <algorithm>
 #include <deque>
 
-using namespace std;
+int r,g,b;
+
+void changeColor()
+{
+    int color = rand()%12+1;
+
+    switch( color )
+    {
+        case 1: //red
+            r = 255; g = 0; b = 0;
+            break;
+        case 2: //orange
+            r = 255; g = 128; b = 0;
+            break;
+        case 3: //yellow
+            r = 255; g = 255; b = 0;
+            break;
+        case 4: //lime
+            r = 128; g = 255; b = 0;
+            break;
+        case 5: //green
+            r = 0; g = 255; b = 0;
+            break;
+        case 6: //blue-green
+            r = 0; g = 255; b = 128;
+            break;
+        case 7: //cyan
+            r = 0; g = 255; b = 255;
+            break;
+        case 8: //cerulean
+            r = 0; g = 128; b = 255;
+            break;
+        case 9: //blue
+            r = 0; g = 0; b = 255;
+            break;
+        case 10: //purple
+            r = 127; g = 0; b = 255;
+            break;
+        case 11: //magenta
+            r = 255; g = 0; b = 255;
+            break;
+        case 12: //pink
+            r = 255; g = 0; b = 127;
+            break;    
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -26,17 +71,20 @@ int main(int argc, char* argv[])
 
     bool running = true;
     int dir = 0;
+    
 
-    SDL_Rect head; //initializing of the "snake"
-    head.x=450;
-    head.y=350;
-    head.w=10;
-    head.h=10;
+    SDL_Rect head {450,350,10,10}; //initializing of the "snake"
+    
+    std::deque<SDL_Rect> rq;
+    int size = 0;
+
+    std::vector<SDL_Rect> apples;
+    int numOfApple = 0;
 
     //main game loop
     while(running)
     {
-        while(SDL_PollEvent(&e))
+        while(SDL_PollEvent(&e)) //handle input
         {
             if(e.type == SDL_QUIT) { running = false; }  
             if(e.type == SDL_KEYDOWN)
@@ -55,9 +103,47 @@ int main(int argc, char* argv[])
                     case SDLK_a: dir = LEFT; break;
                     case SDLK_d: dir = RIGHT; break;
 
-                    case SDLK_RETURN: head.x=450; head.y=350; break;
+                    case SDLK_RETURN: head.x=450; head.y=350; dir = DOWN; break;
                 }
             }
+        }
+
+        if( numOfApple == 0 ) //checks if there are no apples in the screen and puts one in a random spot
+        {
+            int x = rand()%90*10;
+            int y = rand()%70*10;
+            int w = 10;
+            int h = 10;
+            apples.emplace_back(SDL_Rect{x,y,w,h});
+
+            numOfApple++;
+        }
+        
+
+        //collision detection
+        std::for_each( apples.begin(), apples.end(), [&](auto& apple){
+            if( (head.x == apple.x) && (head.y == apple.y) )
+            {
+                size += 1;              
+                apple.x = -10;
+                apple.y = -10;
+                numOfApple = 0;  
+            }
+        });
+
+        std::for_each(rq.begin(), rq.end(), [&](auto& snake_segment){
+            if( (head.x == snake_segment.x) && (head.y == snake_segment.y) )
+            {
+                size = 1;
+                dir = STOP;
+            }
+        });
+
+        rq.push_front(head);
+        
+        while(rq.size() > size)
+        {
+            rq.pop_back();
         }
 
 
@@ -101,10 +187,19 @@ int main(int argc, char* argv[])
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+        std::for_each(rq.begin(), rq.end(), [&](auto& snake_segment)
+        {
+            SDL_RenderFillRect(renderer,&snake_segment);
+        });
         SDL_RenderFillRect(renderer, &head);
 
+        SDL_SetRenderDrawColor(renderer, 255,0,0,255);
+        std::for_each( apples.begin(), apples.end(), [&](auto& apple){
+            SDL_RenderFillRect(renderer, &apple);
+        });
+
         SDL_RenderPresent(renderer);
-        SDL_Delay(40);
+        SDL_Delay(100);
 
     }
 
