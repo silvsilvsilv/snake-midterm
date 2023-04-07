c7ea87e62346e7546f0158e257f869643048ec75
silvsilvsilv <johnleonilsilva24@gmail.com>
2023-04-08 01:09:16 +0800

    improved apple placement, now doesn't place apples at the border of the windows

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,57 +3,64 @@
 #include <vector>
 #include <algorithm>
 #include <deque>
+#include <iostream>
 
 #define WINDOW_WIDTH 900
 #define WINDOW_LENGTH 700
+#define null ' '
 
-int r,g,b;
-
-void changeColor()
+class Color
 {
-    int color = rand()%12+1;
+    public:
+        int r,g,b;
 
-    switch( color )
-    {
-        case 1: //red
-            r = 255; g = 0; b = 0;
-            break;
-        case 2: //orange
-            r = 255; g = 128; b = 0;
-            break;
-        case 3: //yellow
-            r = 255; g = 255; b = 0;
-            break;
-        case 4: //lime
-            r = 128; g = 255; b = 0;
-            break;
-        case 5: //green
-            r = 0; g = 255; b = 0;
-            break;
-        case 6: //blue-green
-            r = 0; g = 255; b = 128;
-            break;
-        case 7: //cyan
-            r = 0; g = 255; b = 255;
-            break;
-        case 8: //cerulean
-            r = 0; g = 128; b = 255;
-            break;
-        case 9: //blue
-            r = 0; g = 0; b = 255;
-            break;
-        case 10: //purple
-            r = 127; g = 0; b = 255;
-            break;
-        case 11: //magenta
-            r = 255; g = 0; b = 255;
-            break;
-        case 12: //pink
-            r = 255; g = 0; b = 127;
-            break;    
-    }
-}
+        void changeColor()
+            {
+                int color = rand()%12+1;
 
+                switch( color )
+                {
+                    case 1: //red
+                        r = 255; g = 0; b = 0;
+                        break;
+                    case 2: //orange
+                        r = 255; g = 128; b = 0;
+                        break;
+                    case 3: //yellow
+                        r = 255; g = 255; b = 0;
+                        break;
+                    case 4: //lime
+                        r = 128; g = 255; b = 0;
+                        break;
+                    case 5: //green
+                        r = 0; g = 255; b = 0;
+                        break;
+                    case 6: //blue-green
+                        r = 0; g = 255; b = 128;
+                        break;
+                    case 7: //cyan
+                        r = 0; g = 255; b = 255;
+                        break;
+                    case 8: //cerulean
+                        r = 0; g = 128; b = 255;
+                        break;
+                    case 9: //blue
+                        r = 0; g = 0; b = 255;
+                        break;
+                    case 10: //purple
+                        r = 127; g = 0; b = 255;
+                        break;
+                    case 11: //magenta
+                        r = 255; g = 0; b = 255;
+                        break;
+                    case 12: //pink
+                        r = 255; g = 0; b = 127;
+                        break;    
+                }
+            }
+};
+
+Color color;
 int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -76,7 +83,7 @@ int main(int argc, char* argv[])
     int dir = 0;
     
 
-    SDL_Rect head {450,350,10,10}; //initializing of the "snake"
+    SDL_Rect head {(WINDOW_WIDTH/2),(WINDOW_LENGTH/2),10,10}; //initializing of the "snake" in the middle of the window
     
     std::deque<SDL_Rect> rq;
     int size = 0;
@@ -95,16 +102,16 @@ int main(int argc, char* argv[])
                 switch(e.key.keysym.sym) //controls 
                 {
                     //arrow controls
-                    case SDLK_DOWN: (dir != UP)? dir = DOWN : ' '; break;
-                    case SDLK_UP: (dir != DOWN)? dir = UP : ' '; break;
-                    case SDLK_LEFT: (dir != RIGHT)? dir = LEFT : ' '; break;
-                    case SDLK_RIGHT: (dir != LEFT)? dir = RIGHT : ' '; break;
+                    case SDLK_DOWN: (dir != UP)? dir = DOWN : null; break;
+                    case SDLK_UP: (dir != DOWN)? dir = UP : null; break;
+                    case SDLK_LEFT: (dir != RIGHT)? dir = LEFT : null; break;
+                    case SDLK_RIGHT: (dir != LEFT)? dir = RIGHT : null; break;
 
-                    //wasd controls like real gamer 8)
-                    case SDLK_s: (dir != UP)? dir = DOWN : ' '; break;
-                    case SDLK_w: (dir != DOWN)? dir = UP : ' '; break;
-                    case SDLK_a: (dir != RIGHT)? dir = LEFT : ' '; break;
-                    case SDLK_d: (dir != LEFT)? dir = RIGHT : ' '; break;
+                    //wasd controls
+                    case SDLK_s: (dir != UP)? dir = DOWN : null; break;
+                    case SDLK_w: (dir != DOWN)? dir = UP : null; break;
+                    case SDLK_a: (dir != RIGHT)? dir = LEFT : null; break;
+                    case SDLK_d: (dir != LEFT)? dir = RIGHT : null; break;
 
                     case SDLK_RETURN: head.x=450; head.y=350; dir = DOWN; break;
                 }
@@ -113,12 +120,13 @@ int main(int argc, char* argv[])
 
         if( numOfApple == 0 ) //checks if there are no apples in the screen and puts one in a random spot
         {
-            int x = (rand()%88)*10;
-            int y = (rand()%68)*10;
+            int x = (rand()%88*10) + 10 ;
+            int y = (rand()%68*10) + 10;
             int w = 10;
             int h = 10;
-            apples.emplace_back(SDL_Rect{x,y,w,h});
+            apples.emplace_back(SDL_Rect{x, y,w,h});
 
+            // std::cout << "X is: " << x << "\nY is: " << y << "\n\n";
             numOfApple++;
         }
         
@@ -134,6 +142,8 @@ int main(int argc, char* argv[])
             }
         });
 
+        //handles death conditions for snake
+        //this handles if snake has collided with its own body
         std::for_each(rq.begin(), rq.end(), [&](auto& snake_segment){
             if( (head.x == snake_segment.x) && (head.y == snake_segment.y) )
             {
@@ -142,16 +152,7 @@ int main(int argc, char* argv[])
             }
         });
 
-        rq.push_front(head);
-        
-        while(rq.size() > size)
-        {
-            rq.pop_back();
-        }
-
-
-        //TEMP: handles the death of the snake when hitting the corners
-        // will add the death condition for when it hits its own body part
+        // this handles if the snake has hit the borders
         if( head.x >= 890 )
         {
             dir = STOP;
@@ -173,6 +174,15 @@ int main(int argc, char* argv[])
             head.y = 0;
         }
 
+        //make snake go big
+        rq.push_front(head);
+        
+        //prevents snake from being longer than the size variable
+        while(rq.size() > size) 
+        {
+            rq.pop_back();
+        }
+
         //handles the movement
         switch (dir)
         {
@@ -186,25 +196,27 @@ int main(int argc, char* argv[])
             break;
         }
 
-        
+        //bg color
         SDL_SetRenderDrawColor(renderer, 0, 0, 0,255);
         SDL_RenderClear(renderer);
 
-        changeColor();
-        SDL_SetRenderDrawColor(renderer, r,g,b,255);
+        //snake color
+        color.changeColor();
+        SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
         std::for_each(rq.begin(), rq.end(), [&](auto& snake_segment)
         {
             SDL_RenderFillRect(renderer,&snake_segment);
         });
         SDL_RenderFillRect(renderer, &head);
 
+        // apple color
         SDL_SetRenderDrawColor(renderer, 255,0,0,255);
         std::for_each( apples.begin(), apples.end(), [&](auto& apple){
             SDL_RenderFillRect(renderer, &apple);
         });
 
-        SDL_RenderPresent(renderer);
-        SDL_Delay(100 - size);
+        SDL_RenderPresent(renderer);//refreshes the window
+        SDL_Delay(100 - size*5 );//sets how fast the speed of the game is
 
     }
 
